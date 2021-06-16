06bd6c615e54de2cc99726e0609b3889346b0c75
plaurion1989 <plaurion@protonmail.com>
2021-06-16 02:16:47 -0700

    added unfinished weather component

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+class Weather extends React.Component{
+
+  render(){
+    return(
+      <>
+      <Card>
+      <h1>Weather Crap</h1>
+      </Card>
+      </>
+    )
+  }
+}
+
+export default Weather;
