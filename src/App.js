2592c0a2342adb875c667a4f03b014fdd573264b
plaurion1989 <plaurion@protonmail.com>
2021-06-15 17:44:29 -0700

    connected locationIQ and rendering location data in a jumbotron

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
+import MyForm from './MyForm.js';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {},
+    }
+  }
+  getLocationData = async (location) => {
 
+    console.log(location);
+
+    let locationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${location}&format=json`);
+
+    console.log(locationData);
+
+    this.setState({
+      location: locationData.data[0]
+    })
+  }
   render() {
     return (
       <>
-      <h1>City Explorer</h1>
-      <h2>Netlify Testing</h2>
+        <h1>City Explorer</h1>
+        <h2>Netlify Testing</h2>
+        <MyForm getLocationData={this.getLocationData} />
+        <Jumbotron>{this.state.location.display_name?<><h3>{this.state.location.display_name}</h3>
+        <p>Latitude:{this.state.location.lat}, Longitude:{this.state.location.lon}</p></>:''}</Jumbotron>
       </>
     )
   }
---
+++ src/MyForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+class MyForm extends React.Component {
+
+  formSubmit = e => {
+    e.preventDefault();
+    this.props.getLocationData(e.target.location.value)
+  }
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.formSubmit}>
+          <Form.Group controlId="location">
+            <Form.Label>Search Cities by Name</Form.Label>
+            <Form.Control placeholder="Enter City Name" />
+            <Form.Text className="text-muted">
+              Find your cheating husband ANYWHERE ON EARTH!
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            FIND HIM!
+          </Button>
+        </Form>
+      </>
+    )
+  }
+}
+
+export default MyForm;
