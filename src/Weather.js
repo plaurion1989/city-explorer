9d13134615a720cc31e784d10a767226f42e979d
plaurion1989 <plaurion@protonmail.com>
2021-08-24 23:18:16 -0700

    finished rendering data from weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,22 @@
 
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 
-class Weather extends React.Component{
-  
-  render(){
-    return(
+class Weather extends React.Component {
+
+  render() {
+    return (
       <>
-      <Card style={this.props.style} >
-      <h1>Weather Crap</h1>
-      </Card>
+      <Row>
+        {this.props.weather.map(day => {
+          return <Card style={{color:'black', width:'185px', marginLeft:'20px'}}>
+            <Card.Title>Date: {day.date}</Card.Title>
+            <Card.Text>Tempratures will be a {day.description}</Card.Text>
+          </Card>
+        })
+        }
+        </Row>
       </>
     )
   }
